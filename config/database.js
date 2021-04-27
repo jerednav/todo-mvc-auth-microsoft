27a3fc0cc58eb4ad60ab1f665b437e9bb0d6b9c4
jerednav <80506833+jerednav@users.noreply.github.com>
2021-04-27 08:34:46 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose')
 //connects to MongoDB through Mongooose
 //MongoDB = database
 //mongo atlas = hosting the database, create a new project
+
+//need to know 2 things:
+//1. using a DB string, the unique string to my database that Mongo Atlas is provisioning for me
+//    DB string is found in .env file to hide from others
+//2. the console.log lets uw know that we are connected if the server is working
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING, {
